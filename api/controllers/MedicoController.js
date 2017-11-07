526ea2ad286a00d665720756e1ac5d4485379845
Jose Guerrero <joseaguerrero6@gmail.com>
2017-11-07 13:31:55 -0500

    controladores y modelos

---
+++ api/controllers/MedicoController.js
@@ -0,0 +1,11 @@
+/**
+ * MedicoController
+ *
+ * @description :: Server-side logic for managing medicoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/PacienteController.js
@@ -0,0 +1,11 @@
+/**
+ * PacienteController
+ *
+ * @description :: Server-side logic for managing pacientes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
